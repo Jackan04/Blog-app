a108e9e28f9b4b6ec366e3894309d07610276790
jackan04 <jacob.asker@icloud.com>
2026-02-10 13:02:55 +0100

    refactor: use class property for port in Database class

--- services/database.js
+++ services/database.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
 
 export default class Database {
+  port = process.env.PORT || 3000;
   app;
   uri = process.env.MONGODB_URI;
 
@@ -16,7 +16,9 @@ export default class Database {
     try {
       await mongoose.connect(this.uri);
       console.log("Connected to MongoDB");
-      this.app.listen(PORT);
+      this.app.listen(this.port, () => {
+        console.log(`Server is running on port ${this.port}`);
+      });
     } catch (error) {
       console.error("MongoDB connection error:", error);
     }
